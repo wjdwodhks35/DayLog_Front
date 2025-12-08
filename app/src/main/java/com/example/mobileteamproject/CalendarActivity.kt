a69e0a92c663cf5468ad958aa0fc5494b68c3faa
정재완 <110615719+wjdwodhks35@users.noreply.github.com>
2025-12-08 15:50:10 +0900

    버그 수정

--- app/src/main/java/com/example/mobileteamproject/CalendarActivity.kt
+++ app/src/main/java/com/example/mobileteamproject/CalendarActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.calendar)
+        setContentView(R.layout.calendar2)
     }
 }
